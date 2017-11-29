a15bcb269d283f9d7bbf5e28329a40ae7c5531c7
leandro <leandrogarber@gmail.com>
2017-11-29 02:17:52 -0300

    first commit

---
+++ boton.gd
@@ -0,0 +1,26 @@
+extends TextureButton
+
+export var colorInactivo = Color(0.8,0.8,0.8)
+export var colorActivo = Color(1,1,1)
+
+signal apretado
+
+func _ready():
+	set_modulate( colorInactivo )
+
+func _on_botonlb_button_down():
+	apretar()
+	emit_signal( "apretado" )
+
+func _on_botonlb_button_up():
+	desapretar()
+
+func apretar():
+	set_modulate( colorActivo )
+	get_node("../sonido").play("sonido")
+
+func desapretar():
+	set_modulate( colorInactivo )
+
+func _on_botonlb_apretado():
+	pass # replace with function body
